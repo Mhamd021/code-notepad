25c66ad4361850971927fd6f6db234d3a487cd05
Mhamd021 <mhamdghanoumstr@gmail.com>
2026-05-21 12:19:33 +0300

    fix n5

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = Number(process.env.PORT) || 3001;
 
   app.enableCors({
     origin: '*',
     credentials: false,
   });
 
-  await app.listen(3001);
+  await app.listen(port);
 }
 bootstrap();
