ea70072dcddad99cd7a091aab0c480a6aaed8db3
Mhamd021 <mhamdghanoumstr@gmail.com>
2026-05-21 11:13:06 +0300

    feat: add netlify config

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
-  await app.listen(3001);
+  
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://code-notepad.vercel.app', 
+      /\.vercel\.app$/,  
+    ],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
-//comment to commit
